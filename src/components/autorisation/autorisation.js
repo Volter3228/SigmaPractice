7ccc6761c17e835bc32d017cc62b12cc0d166eeb
AtlasX1 <pavlo.sergij@ukr.net>
2020-02-01 12:16:46 +0200

    fix bug

--- src/components/autorisation/autorisation.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./autorisation.css";
-import { Container } from "react-bootstrap";
-export default class AutorisationPage extends React.Component {
-  render() {
-    return <Container style={{ paddingTop: "100px" }}>autorisation</Container>;
-  }
-}
--- src/components/autorisation/registration.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./registration.css";
-import { Container } from "react-bootstrap";
-export default class LandigPage extends React.Component {
-  render() {
-    return <Container>registration</Container>;
-  }
-}
