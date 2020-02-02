a7f8e06c7dc62f6330cdddad32ac510dd113e40f
AtlasX1 <pavlo.sergij@ukr.net>
2020-02-02 16:33:07 +0200

    fix bug authorization

--- src/components/landingPage.js
+++ src/components/landingPage.js
@@ -32,18 +32,34 @@ async function getTokenAsync(values, updateAdmin) {
   });
   const data = await response.json();
   if (response.ok === true) {
-    // сохраняем в хранилище sessionStorage токен доступа
     window.location = "/AdminPage";
     sessionStorage.setItem(tokenKey, data.access_token);
     console.log(data.access_token);
   } else {
-    // если произошла ошибка, из errorText получаем текст ошибки
     console.log("Error: ", response.status, data.errorText);
   }
 }
+async function getDataFromApi(url) {
+  const token = sessionStorage.getItem("accessToken");
+  console.log(token);
+  console.log(1);
+  const response = await fetch(url, {
+    method: "GET",
 
+    headers: {
+      Accept: "application/json",
+      Authorization: "Bearer " + token
+    }
+  });
+  console.log(2);
 
+  if (response.ok === true) {
+    let data = await response.json();
 
+    console.log(data);
+    return data;
+  } else console.log("Status: ", response.status);
+}
 const MainPage = () => {
   return (
     <Container style={{ paddingTop: "150px" }}>
@@ -84,7 +100,7 @@ export default class LandingPage extends React.Component {
             <RegistrationPage />
           </Route>
           <Route exact path="/AdminPage">
-            <AdminPage />
+            <AdminPage getDataFromApi={getDataFromApi} />
           </Route>
         </div>
       </Router>
--- src/components/profile/adminPage.js
+++ src/components/profile/adminPage.js
@@ -1,36 +1,11 @@
 import React from "react";
-
 import { Container, Button } from "react-bootstrap";
-async function getData(url) {
-  const token = sessionStorage.getItem("accessToken");
-  console.log(token);
-  console.log(1);
-  const response = await fetch(url, {
-    method: "GET",
 
-    headers: {
-      Accept: "application/json",
-      Authorization: "Bearer " + token
-
-      // "Access-Control-Allow-Origin": "*",
-      // "Access-Control-Allow-Credentials": "true",
-      // "Access-Control-Allow-Methods": "GET,HEAD,OPTIONS,POST,PUT",
-      // "Access-Control-Allow-Headers":
-      //   "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
-    }
-  });
-  console.log(2);
-
-  if (response.ok === true) {
-    const data = await response.json();
-    alert(data);
-  } else console.log("Status: ", response.status);
-}
 export default class AdminPage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      login: null
+      login: ""
     };
   }
   render() {
@@ -39,13 +14,25 @@ export default class AdminPage extends React.Component {
         <h1>adminPage</h1>
         <Button
           onClick={() => {
-            getData("https://localhost:44391/api/values/getlogin");
+            let res = this.props.getDataFromApi(
+              "https://localhost:44391/api/values/getlogin"
+            );
+            res.then(values => {
+              this.setState({ login: values });
+            });
+            console.log(res);
           }}
         >
           Get login
         </Button>
         <br />
-        <Button>log out</Button>
+        <Button
+          onClick={() => {
+            window.sessionStorage.removeItem("accessToken");
+          }}
+        >
+          log out
+        </Button>
         <br />
         login:{this.state.login}
       </Container>
